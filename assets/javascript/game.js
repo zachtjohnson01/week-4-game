ac64d22e97744460ecea0f7bd3fddba1d9fe873d
Zach Johnson <zachjohnson@Zachs-MacBook-Air.local>
2017-12-07 07:11:09 -0600

    Adjusted layout. Linked and started javascript file.

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+
+// VARIABLES
+// ======================================================
+
+var characters = ["Obi-Wan Kenobi","Luke Skywalker","Darth Sidious","Darth Maul"];
+
+
+
+// FUNCTIONS
+// ======================================================
+
+
+
+});
+
+
